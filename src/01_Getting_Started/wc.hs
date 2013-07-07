ddaa1b927f5bdad19e2a117ac77430deb4980768
Raymond Tay <raymondtay1974@gmail.com>
2013-07-08 00:05:26 +0800

    Changes

--- src/01_Getting_Started/wc.hs
+++ src/01_Getting_Started/wc.hs
@@ -1,3 +1,10 @@
+import qualified Data.Text as T
+
 main = interact wordCount
-    where wordCount input = show (length (lines input)) ++ "\n"
+    where wordCount input = "length = " ++ show (length (lines input)) ++ ",words in file = " ++ show (wordsInFile (lines input)) ++ ",characters in file = " ++ show (charsInFile (lines input)) ++ "\n"
+          wordsInFile [] = 0
+          wordsInFile (x:xs) = (length (words x)) + wordsInFile xs
+          charsInFile [] = 0
+          charsInFile (x:xs) = let t = T.pack(x) in
+                                    T.length(t) + charsInFile xs
 
