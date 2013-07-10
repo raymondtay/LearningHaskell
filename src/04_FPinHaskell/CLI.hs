507b7b5f5d136be9e6a5d64a6fec444f53e23f34
Raymond Tay <raymondtay1974@gmail.com>
2013-07-10 14:35:27 +0800

    fixed CLI

--- src/04_FPinHaskell/CLI.hs
+++ src/04_FPinHaskell/CLI.hs
@@ -5,7 +5,11 @@ import System.Environment (getArgs)
 interactWith function inputFile outputFile = do
         input <- readFile inputFile
         writeFile outputFile (function input)
-
+-- the 'do' keyword introduces a block of actions
+-- that can cause effects in the real world, such as reading 
+-- or writing a file. The <- operator is the equivalent of assignment
+-- inside a do-block. This is enough explanation to get us 
+-- started. 
 main = mainWith myFunction
     where   myFunction = id 
             mainWith function = do
