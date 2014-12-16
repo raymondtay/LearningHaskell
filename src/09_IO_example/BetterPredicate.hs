ccd991df3fc8b3b55ef0ef9e4f7eeb8ef3d732c7
Raymond Tay <raymondtay1974@gmail.com>
2014-12-17 06:54:39 +0800

    wip

--- src/09_IO_example/BetterPredicate.hs
+++ src/09_IO_example/BetterPredicate.hs
@@ -20,10 +20,10 @@ getFileSize :: FilePath -> IO (Maybe Integer)
 betterFind :: Predicate -> FilePath -> IO [FilePath]
 betterFind predicate path = getRecursiveContents path >>= filterM check
     where check name = do
-        perms    <- getPermissions name
-        size     <- getFileSize name
-        modified <- getModificationTime name
-        return (predicate name perms size modified)
+	        perms    <- getPermissions name
+	        size     <- getFileSize name
+	        modified <- getModificationTime name
+	        return (predicate name perms size modified)
 
 -- `filterM` behaves like the normal filter function, but in this case
 -- it evaluates the predicate in the IO monad, allowing the predicate
@@ -67,4 +67,38 @@ pathP path _ _ _ = path
 sizeP :: InfoP Integer
 sizeP _ _ (Just size) _ = size
 sizeP _ _ Nothing     _ = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k = \w x y z -> f w x y z == k
+
+equalP' :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP' f k w x y z = f w x y z == k
+
+-- we like to be able to write other binary functions. we'd prefer not to write
+-- a complete definition of each one, because that seems unnecessarily verbose.
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g w x y z = f w x y z && g w x y z
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+andP = liftP2(&&)
+orP  = liftP2(||)
+
+constP :: a -> InfoP a
+constP k _ _ _ _ = k
+
+-- In haskell, functions that take other functions as arguments
+-- and return new functions as "combinators"
+
+liftP' q f k w x y z = f w x y z `q` constP k w x y z
+
+myTest path _ (Just size) = takeExtension path = ".cpp" && size > 131072
+myTest _ _ _ _ = False
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath f w _ _ _ = f w
 
---
+++ src/09_IO_example/ControlledVisits.hs
@@ -0,0 +1,43 @@
+
+import System.Directory (Permissions(..))
+import System.Time (ClockTime(..))
+import System.FilePath ((</>))
+import Control.Monad
+import System.Directory
+import System.IO
+import Control.Exception
+
+data Info = Info {
+    infoPath :: FilePath,
+    infoPerms :: Maybe Permissions,
+    infoSize :: Maybe Integer,
+    infoModTime :: Maybe ClockTime
+} deriving (Eq, Ord, Show)
+
+getInfo :: FilePath -> IO Info
+getInfo path = do
+    perms <- maybeIO (getPermissions path)
+    size  <- maybeIO (bracket (openFile path ReadMode) hClose hFileSize)
+    modified <- maybeIO (getModificationTime path)
+    return (Info path perms size modified)
+
+maybeIO :: IO a -> IO (Maybe a)
+maybeIO act = handle (\_ -> return Nothing)(Just `liftM` act)
+
+traverse :: ([Info] -> [Info]) -> FilePath -> IO [Info]
+traverse order path = do
+    names  <- getUsefulContents path
+    contents <- mapM getInfo (path : map (path </>) names)
+    liftM concat $ forM (order contents) $ \info -> do
+        if isDirectory info && infoPath info /= path
+        then traverse order (infoPath info)
+        else return [info]
+
+getUsefulContents :: FilePath -> IO [String]
+getUsefulContents path = do
+    names <- getDirectoryContents path
+    return (filter (`notElem` [".", ".."]) names)
+
+isDirectory :: Info -> Bool
+isDirectory = maybe False searchable . infoPerms
+
