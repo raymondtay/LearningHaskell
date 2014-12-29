5666aa20ecf5455b09aa87ec4055c178529d83a8
Raymond Tay <raymondtay1974@gmail.com>
2014-12-29 12:34:11 +0800

    more stuff on deriving typeclasses in haskell

--- src/06_Typeclasses/newtype.hs
+++ src/06_Typeclasses/newtype.hs
@@ -25,3 +25,42 @@ newtype UniqueID = UniqueID Int
 -- for Eq, Ord and Show. As a result, we can compare and print values of type NewInt.
 
 
+-- data Person = Person { firstName :: String, lastName :: String, age :: Int }
+
+{-
+
+When we change the declaration to the following expression, it now means that we can conduct
+comparisons and how Haskell does that is that it applies the "==" to each field declared within 
+the "Person" structure <=> the types of all the fields also have to be part of the Eq typeclass.
+data Person = Person { firstName :: String, lastName :: String, age :: Int } deriving (Eq)
+
+The next expression is to allow us to literally print out what a data type looks like by deriving 
+our data type from the "Show" typeclass. Following that, we can improve by reading in a string which
+defines our "Person" data type by deriving from the "Read" typeclass.
+
+data Person = Person { firstName :: String, lastName :: String, age :: Int } deriving (Eq, Show, Read)
+*Main> let p11 = read "Person {firstName = \"patrick\", lastName = \"tay\", age = 39}" :: Person
+*Main> show p11
+"Person {firstName = \"patrick\", lastName = \"tay\", age = 39}"
+
+-}
+
+data Person = Person { firstName :: String, lastName :: String, age :: Int } deriving (Eq, Show, Read)
+
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving (Eq, Show, Read, Ord, Bounded, Enum)
+{-
+The previous expression allows us to do the following:
+*Main> succ Monday
+Tuesday
+*Main> succ Saturday
+Sunday
+but doesn't allow us to to do the following:
+*Main> pred Monday
+*** Exception: pred{Day}: tried to take `pred' of first tag in enumeration
+*Main> succ Sunday
+*** Exception: succ{Day}: tried to take `succ' of last tag in enumeration
+so it does look like the Enumeration we've defined in "Day" is such that Monday thru Sunday is presented
+as "monotically increasing" but does not cycle through....i wonder how to solve this?
+-}
+
