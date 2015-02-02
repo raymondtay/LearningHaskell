464f8b39e05afa9edeb701e490726b81112524d2
Raymond Tay <raymondtay1974@gmail.com>
2015-02-02 12:10:05 +0800

    more State monad stuff

--- src/11_functors_monoids_n_cool_shit/statestuff.hs
+++ src/11_functors_monoids_n_cool_shit/statestuff.hs
@@ -1,13 +1,80 @@
+import Control.Monad.State
+import System.Random
+
 type Stack = [Int]
 
-pop :: Stack -> (Int, Stack)
-pop (x: xs) = (x, xs)
+pop :: State Stack Int -- pop takes a state and places the first element in the first position of a 2-tuple
+pop = state $ \(x:xs) -> (x, xs)
 
-push :: Int -> Stack -> ((), Stack)
-push a xs = ((), a:xs)
+push :: Int -> State Stack () -- push takes a number and prepends it to the current stack
+push a = state $ \xs -> ((), a:xs)
 
-stackManip :: Stack -> (Int, Stack)
-stackManip stack = let
-    ((), new1) = push 42 stack
-    (v, new2) = pop new1
-    in pop new2 
+stackManip :: State Stack Int
+stackManip = do
+  push 3
+  a <- pop
+  pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+  a <- pop
+  if a == 5
+  then 
+    push 5
+  else do
+    push 3
+    push 8
+
+getS = state $ \s -> (s,s)
+putS newstate = state $ \_ -> ((), newstate)
+
+anotherStackStuff :: State Stack ()
+anotherStackStuff = do
+    now <- getS
+    if now == [1,2,3]
+    then putS [3,2,1]
+    else putS [4,5,6]
+
+{-
+
+Here's how we could have written a State-Monad instance
+and we start off by defn a new type `StateA` and from there
+we are going to define how we can interact with this state monad
+
+newtype StateA s a = StateA { runState :: s -> (a, s) }
+instance Monad (StateA x) where
+    return x = StateA $ \s -> (x,s)
+    (StateA l) >>= f = StateA $ \s -> let (a, newState) = l s
+                                          (StateA r) = f a
+                                      in r newState
+
+-}
+
+randomCoinThrow = state random
+
+throw3 :: State StdGen (Bool,Bool,Bool)
+throw3 = do
+    a <- randomCoinThrow
+    b <- randomCoinThrow
+    c <- randomCoinThrow
+    return (a,b,c)
+
+{-
+Some useful monadic functions
+
+When we started our journey to the top of Monad mountian,
+we first looked at `functors`, which are for thigns that can be mapped over.
+Then, we learned about improved functors i.e. `applicative functors` which allows us to 
+apply normal funcitons between several applicative values as well as to 
+take a normal value and put it in some default context. Finally,
+we introduced Monads as improved applicative functors, which added the ability for 
+these values with context to somehow be fed into normal funcitons.
+
+So every monad is an applicative functor and every applicative functor is afunctor.
+The `Applicative` typeclass has a class constraint such that our type has to be an 
+instance of `Functor` before we can make it an instance of `Applicative'. But even 
+though `Monad` should have the same constraint for `Applicative`, as every monad is an applicative
+functor, it doesn't, because the Monad tpe class was introduced to haskell way before `Applicative`.
+
+-}
+
