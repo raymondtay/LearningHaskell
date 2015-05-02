637af57cf556df2b57a257fc178490086e3f88be
Raymond Tay <raymondtay1974@gmail.com>
2015-05-02 12:16:49 +0800

    .

--- src/06_Typeclasses/overlapping_instances.hs
+++ src/06_Typeclasses/overlapping_instances.hs
@@ -11,9 +11,18 @@
   instances to choose from, this extension causes the compiler to pick the most specific one.
 -}
 
-{-# LANGUAGE TypeSynonymInstances, OverlappingInstances #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverlappingInstances #-}
 
 import Data.List
+
+class Borked a where
+  bork :: a -> String
+instance Borked Int where
+  bork = show
+instance Borked (Int,Int) where
+  bork (a,b) = bork a ++ " , " ++ bork b
+instance (Borked a, Borked b) => Borked (a,b) where
+  bork (a, b) = ">> " ++ bork a ++ ", " ++ bork b ++ " <<"
 
 class Foo a where
   foo :: a -> String 
@@ -23,4 +32,45 @@ instance Foo a => Foo [a] where
 
 instance Foo Char where
   foo c = [c]
+
+instance Foo String where
+  foo s = id s
+
+{-
+
+The following interpreter sessions illustrate the difference between newtype and data constructors
+A type created with the "data" keyword has a bookkeeping cost at runtime, for example
+in order to track which constructor created a value. A "newtype" value, otoh, can have
+only one constructor and so does not need this overhead. This makes it more space and time 
+efficient at runtime.
+
+Because a newtype's constructor is used only at compile time and does not even
+exist at runtime, pattern matching on undefined behaves differently for types defined
+using "newtype" than for those that use "data".
+
+"undefined" when evaluated at runtime results in crash! so its most appropriate
+in using that in the following interpreter session
+
+*Main> data DataInt = D Int
+*Main> case D undefined of D _ -> 1
+1
+*Main> :t D undefined
+D undefined :: DataInt
+*Main> :t D
+D :: Int -> DataInt
+*Main> newtype NewTypeInt = N Int
+*Main> case N undefined of N _ -> 1
+1
+
+
+We dont crash here because there's no constructor present at runtime, matching against
+N _ is infact equivalent to matching the wildcard (_) and it succeeds everytime
+
+*Main> case undefined of N _ -> 2
+2
+*Main> case undefined of D _ -> 2
+*** Exception: Prelude.undefined
+*Main>
+
+-}
 
