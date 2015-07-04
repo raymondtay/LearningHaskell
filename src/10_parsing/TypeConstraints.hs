4a9eb66be2b704326f9ff1fa0d332a05f5de321a
Raymond Tay <raymondtay1974@gmail.com>
2015-07-04 12:11:16 +0800

    dropping type constraints

--- src/10_parsing/TypeConstraints.hs
+++ src/10_parsing/TypeConstraints.hs
@@ -5,11 +5,11 @@ It has the effect of forcing you to add type constraints to every function that
 will operate on values of that type.
 
 The solution is to omit type constraints from type definitions, and instead place them on 
-the funcitons that need them.
+the functions that need them.
 -}
 
 -- Have to set `:set -XDatatypeContexts` for the following to work
-
+{-
 data (Ord a) => OrdStack a = Bottom
     | Item a (OrdStack a)
     deriving (Show)
@@ -21,5 +21,19 @@ isIncreasing (Item a rest@(Item b _))
 isIncreasing _ = True
  
 push :: (Ord a) => a -> OrdStack a -> OrdStack a
+push a s = Item a s
+-}
+
+data OrdStack a = Bottom
+    | Item a (OrdStack a)
+    deriving (Show)
+
+isIncreasing :: (Ord a) => OrdStack a -> Bool -- we cannot drop the type-constraint here else '<' would not work.
+isIncreasing (Item a rest@(Item b _))
+  | a < b = isIncreasing rest
+  | otherwise = False
+isIncreasing _ = True
+ 
+push :: a -> OrdStack a -> OrdStack a
 push a s = Item a s
 
