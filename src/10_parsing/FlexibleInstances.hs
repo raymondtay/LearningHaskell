b8db79393c9b2087311e4667b6c319b21e184f92
Raymond Tay <raymondtay1974@gmail.com>
2015-07-04 22:53:19 +0800

    .

--- src/10_parsing/FlexibleInstances.hs
+++ src/10_parsing/FlexibleInstances.hs
@@ -11,5 +11,13 @@ instance Functor (Either Int) where
   > Left 1
   fmap (== "cheeseburger") (Right "fries" :: Either Int String)
   > Right False
+
+
+  Two basic rules about functors and they are:
+  + functors must preserve identity. That is, applying `fmap id` to a value 
+    should give us back an identical value.
+  + functors must be composable. That is, composing two uses of
+    fmap should give the same result as one fmap with the same functions composed
+
 -}
 
