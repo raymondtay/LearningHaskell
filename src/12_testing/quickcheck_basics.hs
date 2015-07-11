b2d932522f20c5c2c9cc024f06e67b811c35b5f0
Raymond Tay <raymondtay1974@gmail.com>
2015-07-11 11:14:54 +0800

    QuickCheck stuff

--- src/12_testing/quickcheck_basics.hs
+++ src/12_testing/quickcheck_basics.hs
@@ -1,3 +1,5 @@
+module MyQuickCheck where
+
 import Test.QuickCheck
 import Data.List
 
@@ -17,3 +19,36 @@ prop_idempotent xs = qsort (qsort xs) == qsort xs
 -- (0.04 secs, 13966312 bytes)
 
 -- quickCheck (prop_idempotent :: [Integer] -> Bool)
+
+--
+-- Another function 
+--
+mhead :: [a] -> a
+mhead (x:_) = x
+mhead []    = error "Empty list. Oops"
+
+prop_mhead (x:xs) = mhead (x:xs) == x
+
+-- quickCheck (prop_mhead :: [Integer] -> Bool)
+
+xminimum :: (Ord a) => [a] -> a
+xminimum [] = error "Empty list. Oops"
+xminimum xs = foldl1 min xs
+
+prop_xminimum xs = not (null xs) ==> head (qsort xs) == minimum xs
+-- quickCheck prop_xminimum
+
+prop_ordered xs = ordered (qsort xs)
+  where ordered [] = True
+        ordered [x] = True
+        ordered (x:y:ys) = x <= y && ordered (y:xs)
+-- quickCheck (prop_ordered:: [Integer] -> Bool)
+
+prop_permutation xs = permutation xs (qsort xs)
+  where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+-- quickCheck (prop_permutation:: [Integer] -> Bool)
+
+prop_maximum xs = not (null xs) ==> last (qsort xs) == maximum xs
+
+prop_append xs ys = not (null xs)  && not (null ys) ==> head (qsort (xs ++ ys)) == min (xminimum xs) (xminimum ys)
+
