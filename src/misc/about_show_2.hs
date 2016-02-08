c3737dd67fd965bf3fd88bbc2cf150fd06bbceba
Raymond Tay <raymondtay1974@gmail.com>
2016-02-08 22:20:54 +0800

    .

--- src/misc/about_show_2.hs
+++ src/misc/about_show_2.hs
@@ -32,4 +32,10 @@ instance Ord a => Ord (Identity a) where
   compare (Identity a) (Identity b) = compare a b 
 
 
+-- Remember: Typles have the same syntax for their
+-- type constructors and their data constructors.
+--
+f :: (a,b,c) -> (d,e,f) -> ((a,d), (c,f))
+f (a,b,c) (d,e,f) = ((a,d), (c, f))
+
 
