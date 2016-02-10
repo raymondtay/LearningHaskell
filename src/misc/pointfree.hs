c8d3820e80ecace2cb92c4819cba195040ebf40e
Raymond Tay <raymondtay1974@gmail.com>
2016-02-10 11:08:02 +0800

    .

---
+++ src/misc/pointfree.hs
@@ -0,0 +1,22 @@
+module Pointfree where
+
+{-
+ - It is very common for functional programmers to write functions as a composition of other
+ - functions, never mentioning the actual arguments they will be applied to. 
+ - 
+ -}
+
+sum :: (Num b, Foldable t) => t b -> b
+sum = foldr (+) 0
+
+{-
+ - The above is completely equivalent to 
+ - sum' xs = foldr(+) 0 xs
+ - but the former is more compact. This is very much related to function pipelines 
+ - and its, debatably, clearer to write 
+ - f . g . h than to write, say let fn x = f (g ( h x ))
+ -}
+
+roundtrip :: (Read a, Show a) => a -> a
+roundtrip a = read (show a)
+
