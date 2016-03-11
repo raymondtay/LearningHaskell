0c2524e2d62c6905011390bbf59c05c95142bb54
Raymond Tay <raymondtay1974@gmail.com>
2016-03-11 13:48:11 +0800

    .

--- src/misc/chapter-17-exercises.hs
+++ src/misc/chapter-17-exercises.hs
@@ -12,17 +12,29 @@ z :: Maybe Integer
 z = lookup 2 $ zip [1..3] [4..6]
 
 tupled :: Maybe (Integer, Integer)
-tupled = 
-  case (y, z) of
-    (Nothing, Nothing) -> Nothing
-    (Nothing, _)  -> Nothing
-    (_, Nothing)  -> Nothing
-    (Just x, Just y) -> Just $ (,) x y
+tupled = Just $ (,) (fromMaybe 1 y) (fromMaybe 1 z)
 
+x' :: Maybe Int
+x' = elemIndex 3 [1..5]
 
--- tupled = (, ) <*> y z
---
+y' :: Maybe Int
+y' = elemIndex 4 [1..5]
 
-x :: Maybe Int
-x = elemIndex 3 [1..5]
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = Just $ max' (fromMaybe 1 x') (fromMaybe 1 y')
+
+xs = [1..3]
+ys = [4..6]
+
+a :: Maybe Integer
+a = lookup 3 $ zip xs ys
+
+b :: Maybe Integer
+b = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = sum $ (,) a b
 
