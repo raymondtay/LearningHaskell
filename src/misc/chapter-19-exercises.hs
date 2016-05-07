154db3e112cecb16ea191252f57205fa2f09ec7e
Raymond Tay <raymondtay1974@gmail.com>
2016-05-07 12:24:27 +0800

    stuff from chapter19

---
+++ src/misc/chapter-19-exercises.hs
@@ -0,0 +1,8 @@
+module Chap19 where
+
+import Data.Time.Clock
+
+offsetCurrentTime :: NominalDiffTime -> IO UTCTime
+offsetCurrentTime offset = 
+  fmap (addUTCTime (offset * 24 * 3600)) $ getCurrentTime
+
