5d3e911eb3a649855e3426f04b3550ff158534a2
Raymond Tay <raymondtay1974@gmail.com>
2016-07-02 12:46:48 +0800

    ...

---
+++ src/misc/chapter-25-exercises.hs
@@ -0,0 +1,21 @@
+
+--
+-- `{-# LANGUAGE InstanceSigs #-}` 
+-- Allows type signatures to be specified in 
+-- instance declarations.
+--
+
+{-# LANGUAGE InstanceSigs #-}
+
+newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure = undefined
+
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (Compose f) <*> (Compose a) = undefined
+
