5d93cd5e1bfd8c302a1e4316078e0601287079b6
Raymond Tay <raymondtay1974@gmail.com>
2016-09-28 12:38:15 +0800

    mempty impl

--- src/misc/chapter-15-4-exercises.hs
+++ src/misc/chapter-15-4-exercises.hs
@@ -5,5 +5,11 @@ import Data.Monoid
 newtype Mem s a = Mem { runMem :: s -> (a, s) }
 
 instance Monoid a => Monoid (Mem s a) where
+  mempty = Mem $ \s -> (mempty, s)
   mappend msa msa' = Mem $ \_s -> (mappend (fst (runMem msa $ _s)) (fst (runMem msa' $ _s)), _s)
+
+f' = Mem $ \s -> ("hi", s + 1)
+
+main = do
+  print $ runMem (f' <> mempty) 0
 
