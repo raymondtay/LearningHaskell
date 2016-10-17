43e6e565bb28ad39ba6c0e160562e9c6b5cfc2af
Raymond Tay <raymondtay1974@gmail.com>
2016-10-17 22:53:27 +0800

    stuff

--- src/misc/README-chap-25.md
+++ src/misc/README-chap-25.md
@@ -12,19 +12,17 @@ means that you can compose two functors (or two applicatives) And return
 another functor (or applicative). This is not true of Monads, however:
 when you compose two monads, the result is not necessarily another Monad.
 
+A Monad Transformer is a variant of an ordinary type that takes 
+an additional type argument which is assumed to have a monad instance.
+For example, _MaybeT_ is the transformer variant of the _Maybe_ type.
+The transformer variant of a type gives us a _Monad_ instance that binds
+over both bits of structure. This allows us to compose monads and combine
+their effects. Gettig comfortable with monad transformers is important
+to becoming proficient in Haskell, so we are going to take it pretty slowly
+and go step by step. You won't necessarily want to start out early on defining
+a bunch of transformer stacks yourself, but getting familiar is always advantageous.
 
 The important thing is that Monad Transformers are never sum or product types;
 they are always just a means of wrapping one extra layer of (monadic) structure 
 around a type, so there is never a reason they couldn't be newtypes.
-
-
-
-
-
-
-
-
-
-
-
 
--- src/misc/chapter-25-exercises.hs
+++ src/misc/chapter-25-exercises.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Chap25 where
 
 --
 -- `{-# LANGUAGE InstanceSigs #-}` 
@@ -5,17 +8,54 @@
 -- instance declarations.
 --
 
-{-# LANGUAGE InstanceSigs #-}
-
+-- 
+-- Over here, this represents a type constructors, not term-level expressions
+--
 newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
 
 instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
 
+-- 
+-- Sample usage: 
+-- (\x -> x + 1) <$> Compose [Just 1]
+-- Compose { getCompose = [Just 2] }
+-- 
+-- From the above, we can see that (\x -> x + 1) is lifted twice into `Compose`
+-- to apply the lambda to the value of '2' (parlance would be `..mapped over the value..`)
+-- inside the `Maybe` and doesn't alter
+-- the structure at all; hence we see its `Compose { getCompose = [Just 2]}`.
+--
+--
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
-  -- pure :: a -> Compose f g a
-  pure = Compose $ getCompose
-
+  pure :: a -> Compose f g a
+  pure = Compose . pure . pure -- why is 'pure' used? because the declaration already said that f ∈ Applicative, g ∈ Applicative.
+                               -- and also type of `pure . pure :: (Applicative f, Applicative f1) => a -> f (f1 a)`
   (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
-  (Compose f) <*> (Compose a) = undefined
+  (Compose f') <*> (Compose a') = Compose $ ((<*>) <$> f') <*> a'
+
+
+-- 
+-- Here's how a multi-layered functor can be potentially 
+-- expressed.
+--
+newtype One f a = One (f a) deriving (Eq, Show)
+instance Functor f => Functor (One f) where
+  fmap f (One fa) = One $ fmap f fa
+
+newtype Three f g h a = Three (f (g (h a))) deriving (Eq, Show)
+instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where
+  fmap f (Three fgha) = Three $ (fmap . fmap . fmap) f fgha
+
+--
+-- An example using `Compose` would be the following:
+--
+-- *Chap25 System.Random Control.Applicative> let v = Compose [Just (Compose $ Just [1])]
+-- *Chap25 System.Random Control.Applicative> :t v
+-- v :: Num a => Compose [] Maybe (Compose Maybe [] a)
+--
+--
+
+
+
 
