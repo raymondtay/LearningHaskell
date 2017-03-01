c3d4328ebb483e270b3486e6687d468058f61be4
Raymond Tay <raymondtay1974@gmail.com>
2017-03-01 09:55:15 +0800

    more stuff

--- src/HaskellBook/Ch16/exercises.hs
+++ src/HaskellBook/Ch16/exercises.hs
@@ -117,3 +117,80 @@ instance Functor (Flip K a) where
 -- provide a type parameter, say 'a'. Then after that, we are good to apply the
 -- usual understanding of how to define `fmap` for Flip K instances.
 --
+
+data EvilGoateeConst a b = GoatyConst b
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+-- I know that whenever i see something like LiftItOut (f a) then 'f' is
+-- actually a function and together with (f a) it means that its function
+-- application. therefore, we now can actually lift out this function
+-- application 'fa' via fmapping it but the requirement is that the function
+-- that's embedded in 'fa' must actually be a Functor .... which explains the
+-- final signature.
+--
+data LiftItOut f a = LiftItOut (f a) deriving Show
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut fa) = LiftItOut (fmap f fa)
+
+-- To understand the following code snippet, i again apply the reasoning i used
+-- before in the last code for `LiftItOut` and the same rationale applies.
+--
+data Parappa f g a = DaWrappa (f a) (g a)
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa fa ga) = DaWrappa (fmap f fa) (fmap f ga)
+
+
+-- To understand this code snippet, the main thing is to realize that i can
+-- safely ignore fmapping over 'fa' because its actually part of the functor
+-- definition and we should focus on the 'b' since that's going to be subjected
+-- to fmapping and its actually embedded in 'gb' (because we know 'gb' eqv. (g b))
+-- and to fmap over 'gb' means that g ∈ Functor.
+--
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b) 
+instance Functor g => Functor (IgnoreOne f g a) where
+  fmap f (IgnoringSomething fa gb) = IgnoringSomething fa (fmap f gb)
+
+
+
+-- Again, the following example demonstrates how we should always keep our
+-- focus on the type parameter that is not bounded to the functor definition
+-- and from there, we know (almost by instinct) that i need to fmap over 'gt'.
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious go ga gt) = Notorious go ga (fmap f gt)
+
+-- This is slightly trickier than the previous ones. The tricky part is that
+-- the data constructor "seems" to be the same as the type constructor. In
+-- Haskell, we need to remember that they are two different things though they
+-- look exactly the same.
+-- 
+-- In here, i used 't' to represent (List a) since it literally is the tail of
+-- the list. Next, i fmap over 't' and i do not need to place a type constraint
+-- in the declaration of the functor because its already there. i.e. List ∈
+-- Functor.
+data List a = Nil | Cons a (List a) 
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons h t) = Cons (f h) (fmap f t)
+
+
+-- This should be easier to understand now - if you had understood the previous
+-- examples.
+data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a) 
+instance Functor GoatLord where
+  fmap f NoGoat = NoGoat
+  fmap f (OneGoat a) = OneGoat (f a) 
+  fmap f (MoreGoats aa bb cc) = MoreGoats (fmap f aa) (fmap f bb) (fmap f cc)
+
+-- 
+-- Prelude defines type String = [Char]
+-- 
+-- data TalkToMe a = Halt | Print String a | Read (String -> a)
+-- 
+data TalkToMe a = Halt | Print [Char] a | Read ([Char] -> a)
+instance Functor TalkToMe where
+  fmap f Halt = Halt
+  fmap f (Print s a) = Print s (f a)
+  fmap f (Read sa) = Read (f . sa)
+
