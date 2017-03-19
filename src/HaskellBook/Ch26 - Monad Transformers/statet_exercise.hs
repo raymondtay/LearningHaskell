0aa776f790539b24b55fc0095fe89a35a4a5ed17
Raymond Tay <raymondtay1974@gmail.com>
2017-03-19 15:16:13 +0800

    Applicative instance for StateT

--- src/HaskellBook/Ch26 - Monad Transformers/statet_exercise.hs
+++ src/HaskellBook/Ch26 - Monad Transformers/statet_exercise.hs
@@ -44,7 +44,18 @@ instance Functor m => Functor (StateT s m) where
 --
 -- (\s -> pure (a , s)) <--- Haskell knows that the 'pure' refers to the 'm'.
 --
-instance Applicative m => Applicative (StateT s m) where
+instance Monad m => Applicative (StateT s m) where
   pure :: a -> StateT s m a
   pure a = StateT $ (\s -> pure (a, s))
+
+  (<*>) :: StateT s m (a -> b) -> StateT s m a -> StateT s m b
+  (StateT f) <*> (StateT ma) =
+    -- f :: s -> m ((a -> b), s)
+    -- ma :: s -> m (a, s)
+    StateT $
+      (\s -> do
+        ff <- (f s) -- ff :: ((a->b), s)
+        gg <- (ma s) -- gg :: (a, s)
+        return ((fst ff) (fst gg), s))
+
 
