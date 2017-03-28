d987514ff74e1b7dc73360a1cc88acd4a1a957d8
Raymond Tay <raymondtay1974@gmail.com>
2017-03-28 17:28:27 +0800

    more stuff

--- src/HaskellBook/Ch27 - Non-strictness/strict_nonstrict_data.hs
+++ src/HaskellBook/Ch27 - Non-strictness/strict_nonstrict_data.hs
@@ -12,4 +12,32 @@ main = print (blah undefined)
 -- case basis.
 --
 
+willForce x = 1
+willNotForce ~x = 1
 
+-- our List and sTake is lazy here. 
+--
+data List a = Nil | Cons a (List a) deriving Show
+
+sTake :: Int -> List a -> List a
+sTake n _ 
+  | n <= 0 = Nil
+sTake n Nil = Nil
+sTake n (Cons x xs) = (Cons x (sTake (n-1) xs))
+
+twoEls = Cons 1 (Cons undefined Nil)
+oneEl = sTake 1 twoEls
+
+-- Noting the placement of the exclamation marks denoting strictness, run the
+-- entire code base in GHCi and see what happens.
+--
+data List' a = Nil'| Cons' !a (List' a) deriving Show
+
+sTake' :: Int -> List a -> List a
+sTake' n _ 
+  | n <= 0 = Nil
+sTake' n Nil = Nil
+sTake' n (Cons x xs) = (Cons x (sTake' (n-1) xs))
+
+twoEls' = Cons 1 (Cons undefined Nil)
+oneEl' = sTake' 1 twoEls'
