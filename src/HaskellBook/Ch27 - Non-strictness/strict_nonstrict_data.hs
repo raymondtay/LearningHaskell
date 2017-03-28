cf032cb5066ec3664a479a38724d28bf681399ec
Raymond Tay <raymondtay1974@gmail.com>
2017-03-28 22:21:46 +0800

    stuff

--- src/HaskellBook/Ch27 - Non-strictness/strict_nonstrict_data.hs
+++ src/HaskellBook/Ch27 - Non-strictness/strict_nonstrict_data.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Strict #-}
+{-# LANGUAGE BangPatterns #-}
 
 module StrictTest where
 
@@ -33,11 +34,24 @@ oneEl = sTake 1 twoEls
 --
 data List' a = Nil'| Cons' !a (List' a) deriving Show
 
-sTake' :: Int -> List a -> List a
+sTake' :: Int -> List' a -> List' a
 sTake' n _ 
-  | n <= 0 = Nil
-sTake' n Nil = Nil
-sTake' n (Cons x xs) = (Cons x (sTake' (n-1) xs))
+  | n <= 0 = Nil'
+sTake' n Nil' = Nil'
+sTake' n (Cons' x xs) = (Cons' x (sTake' (n-1) xs))
 
-twoEls' = Cons 1 (Cons undefined Nil)
+twoEls' = Cons' 1 (Cons' undefined Nil')
 oneEl' = sTake' 1 twoEls'
+
+data List'' a = Nil'' | Cons'' !a (List'' a) deriving Show
+
+-- the strict evaluation is right now applied to "xs" and it would evident when
+-- you play around 
+sTake'' :: Int -> List'' a -> List'' a
+sTake'' n _ | n <= 0 = Nil''
+sTake'' n Nil'' = Nil''
+sTake'' n (Cons'' x !xs) = (Cons'' x (sTake'' (n-1) xs))
+
+twoEls'' = Cons'' 1 (Cons'' undefined Nil'')
+oneEl'' = sTake'' 1 twoEls''
+
