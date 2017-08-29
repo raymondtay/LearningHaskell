946bac2ba9cd029fed8118552974d7b00d942ff4
Raymond Tay <raymond.tay@traditionasia.com>
2017-08-29 17:17:32 +0800

    Recursive types via 'Fix' for Lists

--- src/recursion_schemes/01_basics.hs
+++ src/recursion_schemes/01_basics.hs
@@ -7,12 +7,15 @@
 --  `\case -> ...` and does not bind the 'x' to a name.
 --
 
-import Data.Functor.Foldable
+import Data.Functor.Foldable hiding (ListF)
 import Data.List.Ordered (merge)
 import Prelude hiding (Foldable, succ)
 
 data NatF a = ZeroF | SuccF a deriving (Show, Functor)
+data ListF a r = NilF | ConsF a r deriving (Show, Functor)
+
 type Nat = Fix NatF -- Declaring that `Nat` is a type alias to a 'recursive defn of NatF'
+type List a = Fix (ListF a)
 
 zero :: Nat 
 zero = Fix ZeroF
@@ -36,4 +39,18 @@ natsum' :: Nat -> Int
 natsum' = cata $ \case
   ZeroF   -> 0
   SuccF n -> n + 1
+
+
+nil :: List a 
+nil = Fix NilF
+
+cons :: a -> List a -> List a
+cons x xs = Fix (ConsF x xs)
+
+filterL :: (a -> Bool) -> List a -> List a
+filterL p = cata alg where
+  alg NilF = nil
+  alg (ConsF x xs) 
+    | p x = cons x xs
+    | otherwise = xs
 
