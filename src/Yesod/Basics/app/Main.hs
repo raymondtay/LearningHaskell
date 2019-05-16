63cbb693ef411b32da5611004b4573c7b371c96a
Raymond TAY <raymondtay1974@gmail.com>
2019-05-16 18:38:59 +0200

    Understanding the basics of Hamlet syntax

--- src/Yesod/Basics/app/Main.hs
+++ src/Yesod/Basics/app/Main.hs
@@ -1,5 +1,9 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
-{-# OPTIONS_GHC -ddump-splices #-}
+{-# LANGUAGE
+    ExtendedDefaultRules,
+    OverloadedStrings,
+    QuasiQuotes,
+    TemplateHaskell,
+    TypeFamilies #-}
 
 module Main where
 
@@ -14,6 +18,8 @@ mkYesod "Links" [parseRoutes|
  / HomeR GET
  /page1 Page1R GET
  /page2 Page2R GET
+ /page3 Page3R GET
+ /page4 Page4R GET
  |]
 
 -- instance Yesod HelloWorld
@@ -24,7 +30,10 @@ instance Yesod Links
 getHomeR  = defaultLayout [whamlet|<a href=@{Page1R}> Go to page 1!|]
 getPage1R = defaultLayout [whamlet|<a href=@{Page2R}>Go to page 2!|]
 getPage2R = defaultLayout [whamlet|<a href=@{HomeR}>Go to page 1!|]
+getPage3R = return $ object ["msg" .= "Hello,World"] -- rendering as JSON
+getPage4R = defaultLayout [whamlet| #{renderPerson}|]
 
 main :: IO ()
 main = warp 3000 Links
+
 
--- src/Yesod/Basics/src/Lib.hs
+++ src/Yesod/Basics/src/Lib.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+
 module Lib
-    ( someFunc
+    ( someFunc     ,
+      renderPerson
     ) where
+
+import Text.Hamlet (shamlet, hamlet, HtmlUrl)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import Data.Char (toLower)
+import Data.List (sort)
+import Data.Text (Text)
+
+
+
+data MyRoute = SpecialRoute
+
+data Person = Person { name :: String, age :: Int }
+
+person = Person { name = "Ray", age = 44 }
+
+renderPerson = renderHtml [shamlet|
+<p>Hello, my name is #{name person} and I am #{show $ age person}.
+<p>
+  let's do some funny stuff with my name :#
+  <b>#{sort $ map toLower (name person)}
+  <p>Oh, and in 5 years i will be #{show ((+) 5 (age person))} years old.
+|]
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
