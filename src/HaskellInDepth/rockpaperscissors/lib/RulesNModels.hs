0927f594f943fa22043ec7094fced10c1b19003f
Raymond TAY <raymondtay1974@gmail.com>
2019-09-06 09:20:37 +0800

    Fixed issues

--- src/HaskellInDepth/rockpaperscissors/lib/RulesNModels.hs
+++ src/HaskellInDepth/rockpaperscissors/lib/RulesNModels.hs
@@ -1,5 +1,5 @@
 
-module RulesNModels () where
+module RulesNModels where
 
 import Data.List (group, sort)
 import Control.Monad (replicateM)
@@ -20,6 +20,21 @@ instance Random Weapon where
   randomR (a, b) g =
     case randomR (fromEnum a, fromEnum b) g of
       (r, g') -> (toEnum r, g')
-      random g = randomR (minBound, maxBound) g
+  random g = randomR (minBound, maxBound) g
+
+-- generate weapons rather randomly
+randomWeapon :: State StdGen Weapon
+randomWeapon = state random 
+
+-- 1-game round and get a pair of weapons, randomly by design but that details
+-- is unknown to the fellas here.
+gameRound :: State StdGen (Weapon, Weapon)
+gameRound = (,) <$> randomWeapon <*> randomWeapon
+
+game :: Int -> State StdGen [(Winner, Int)]
+game n = counts <$> replicateM n (winner <$> gameRound)
+  where
+    counts xs = map (\xs@(x:_) -> (x, length xs)) $ group $ sort xs
+
 
 
--- src/HaskellInDepth/rockpaperscissors/src/Main.hs
+++ src/HaskellInDepth/rockpaperscissors/src/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import RulesNModels
+import System.Random
+import Control.Monad.State (evalState)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  g <- newStdGen
+  let r = evalState (game 20) g
+  print r
+
+
