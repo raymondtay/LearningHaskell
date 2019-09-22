906d393dca76b1890251b1dc886cd1c4aaf4480b
Raymond Tay <raymondtay1974@gmail.com>
2019-09-22 11:53:13 +0800

    building it up

--- src/HaskellInDepth/parsers/lib/Parse.hs
+++ src/HaskellInDepth/parsers/lib/Parse.hs
@@ -13,25 +13,92 @@
   [3] - http://hackage.haskell.org/package/attoparsec-0.13.2.3
 -}
 
-module Parse(parseIP) where
+module Parse(logParser, logFile, parseIP') where
 
-import Data.ByteString
+import Data.ByteString       hiding (count)
 import Data.Attoparsec.ByteString
 import Data.Attoparsec.ByteString.Char8
 import Data.Word
 import Control.Applicative
 import Data.Either
 
+import Data.Time
+
+logFile :: FilePath
+logFile = "data.log"
+
+data Product = Mouse | Keyboard | Speakers | Monitor deriving Show
+data Source = Internet | Friend deriving Show
+
+data LogEntry = 
+  LogEntry {
+    entryTime :: LocalTime, 
+    entryIP :: IP,
+    entryProduct :: Product,
+    source :: Source
+  } deriving Show
+
+type Log = [LogEntry] -- list of log entries
+
+timeParser :: Parser LocalTime
+timeParser = do
+  y  <- count 4 digit
+  char '-'
+  mm <- count 2 digit
+  char '-'
+  d  <- count 2 digit
+  char ' '
+  h  <- count 2 digit
+  char ':'
+  m  <- count 2 digit
+  char ':'
+  s  <- count 2 digit
+  return $
+    LocalTime { localDay = fromGregorian (read y) (read mm) (read d),
+                localTimeOfDay = TimeOfDay (read h) (read m) (read s)
+              }
 
 -- Word8 represents 8-bit unsigned integer values
 data IP = IP Word8 Word8 Word8 Word8 deriving Show
 
-parseIP :: ByteString -> IP
-parseIP datum = 
+parseIP' :: ByteString -> IP
+parseIP' datum = 
   let
     _parser = (sepBy1 (many1 decimal) (char '.'))
     parsedS = fromRight ([[0],[0],[0],[0]]::[[Word8]]) (parseOnly _parser datum)
   in IP (parsedS !! 0 !! 0) (parsedS !! 1 !! 0) (parsedS !! 2 !! 0) (parsedS !! 3 !! 0)
 
+parseIP :: Parser IP
+parseIP = do
+  b1 <- decimal
+  char '.'
+  b2 <- decimal
+  char '.'
+  b3 <- decimal
+  char '.'
+  b4 <- decimal
+  return $ IP b1 b2 b3 b4
 
+
+productParser :: Parser Product
+productParser =
+  (string "mouse" >> return Mouse)
+  <|> (string "keyboard" >> return Keyboard)
+  <|> (string "monitor" >> return Monitor)
+  <|> (string "speakers" >> return Speakers)
+
+sourceParser :: Parser Source
+sourceParser =
+  (string "internet" >> return Internet) <|> (string "friend" >> return Friend)
+
+logEntryParser :: Parser LogEntry
+logEntryParser = do
+  t <- timeParser
+  ip <- parseIP
+  prd <- productParser
+  src <- sourceParser
+  return $ LogEntry t ip prd src
+
+logParser :: Parser Log
+logParser = many $ logEntryParser <* endOfLine
 
--- src/HaskellInDepth/parsers/src/Main.hs
+++ src/HaskellInDepth/parsers/src/Main.hs
@@ -2,9 +2,13 @@
 
 module Main where
 
-import Data.ByteString.Char8  (pack) -- bytestring package
+import System.IO                             (print)
+import qualified Data.ByteString.Char8  as B (readFile,unpack,pack) -- bytestring package
+import Data.Attoparsec.ByteString
+import Data.Text
 import Data.Text.Format              -- text-format package
 import Parse
+
 
 main :: IO ()
 main = do
@@ -12,8 +16,10 @@ main = do
       valid0 = "192.168.1.0"
       valid1 = "127.0.0.1"
       preamble = "Before {}, after: [{}]" :: Format
-  putStrLn . show $ (format preamble (invalid0, show . parseIP $ pack (invalid0)))
-  putStrLn . show $ (format preamble (valid0  , show . parseIP $ pack (valid0)))
-  putStrLn . show $ (format preamble (valid1  , show . parseIP $ pack (valid1)))
-
+  putStrLn . show $ (format preamble (invalid0, show . parseIP' $ B.pack (invalid0)))
+  putStrLn . show $ (format preamble (valid0  , show . parseIP' $ B.pack (valid0)))
+  putStrLn . show $ (format preamble (valid1  , show . parseIP' $ B.pack (valid1)))
+  putStrLn "=> Parsing begins..."
+  B.readFile logFile >>= System.IO.print . parseOnly logParser
+  putStrLn "=> Parsing ended."
 
