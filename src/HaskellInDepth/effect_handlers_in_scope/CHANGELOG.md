edb707d879cd68c6e2cbb16fe4779b2f43bcc651
Raymond Tay <raymondtay1974@gmail.com>
2019-10-07 21:46:23 +0800

    effect handlers in scope

---
+++ src/HaskellInDepth/effect_handlers_in_scope/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for effect-handlers-in-scope
+
+## 0.1.0.0 -- 2019-10-06
+
+* First version. Released on an unsuspecting world.
---
+++ src/HaskellInDepth/effect_handlers_in_scope/src/Main.hs
@@ -0,0 +1,43 @@
+{-#LANGUAGE InstanceSigs #-}
+
+-- Directly inspired from the "Effect Handlers in Scope" 2014 paper by Nicholas
+-- Wu, Tom Schrijvers & Ralf Hinze.
+--
+module Main where
+
+data Backtr a =
+  Return a |
+  Fail     |
+  (Backtr a) :| (Backtr a) deriving Show
+
+instance Functor Backtr where
+  fmap f (Return a) = Return (f a)
+  fmap _ Fail = Fail
+  fmap f (p :| q) = (fmap f p) :| (fmap f q)
+
+instance Applicative Backtr where
+  pure = Return
+  (<*>) :: Backtr (a -> b) -> Backtr a -> Backtr b
+  (<*>) (Fail)_    = Fail
+  (<*>) f (p :| q) = (f <*> p) :| (f <*> q)
+
+instance Monad Backtr where
+  return :: a -> Backtr a
+  return = Return
+  (>>=) :: Backtr a -> (a -> Backtr b) -> Backtr b
+  (>>=) Fail       _ = Fail
+  (>>=) (Return a) r = r a
+  (>>=) (p :| q)   r = (p >>= r) :| (q >>= r)
+
+
+knapsack :: Int -> [Int] -> Backtr [Int]
+knapsack w vs | w < 0 = Fail
+              | w == 0 = return []
+              | w > 0 = do v <- select vs
+                           vs <- knapsack (w - v) vs
+                           return (v:vs)
+select :: [a] -> Backtr a
+select = foldr (:|) Fail . map Return
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
