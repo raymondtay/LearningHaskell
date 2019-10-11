b03f2aa757337be1b9efc9816cca3bd4594ed808
Raymond.tay <raymondtay1974@gmail.com>
2019-10-11 09:44:20 +0800

    datatypes alar carte

--- src/HaskellInDepth/effect_handlers_in_scope/lib/Prog.hs
+++ src/HaskellInDepth/effect_handlers_in_scope/lib/Prog.hs
@@ -1,5 +1,9 @@
 
-{-# LANGUAGE DeriveFunctor, InstanceSigs #-}
+{-# LANGUAGE DeriveFunctor,
+             InstanceSigs,
+             TypeOperators,
+             MultiParamTypeClasses,
+             FlexibleInstances #-}
 
 module Prog where
 
@@ -28,10 +32,38 @@ instance (Applicative sig) => Monad (Prog sig) where
   Op op >>= prog = Op(fmap (>>= prog) op)
   
 
-instance Applicative sig => Monad (Prog sig) where
-  return = Return
 
-  Return v >>= prog = prog v
-  Op op >>= prog = Op (fmap (>>= prog) op)
+
+-- Generally speaking, we will be dealing with several different effects that
+-- work together in a single program, and so we need a means of flexibly
+-- composing signatures, where each signature captures syntax that encodes a
+-- particular effect.
+--
+data (sig1 + sig2) cnt = Inl (sig1 cnt) | Inr (sig2 cnt) deriving Functor
+
+class (Functor sub, Functor sup) => sub ⊂ sup where
+  inj :: sub a -> sup a
+  prj :: sup a -> Maybe (sub a)
+
+instance Functor sig => sig ⊂ sig where
+  inj = id
+  prj = Just
+
+instance (Functor sig1, Functor sig2) => sig1 ⊂ (sig1 + sig2) where
+  inj = Inl
+  prj (Inl fa) = Just fa
+  prj _ = Nothing
+
+instance (Functor sig1, sig ⊂ sig2) => sig ⊂ (sig1 + sig2) where
+  inj = Inr . inj
+  prj (Inr ga) = prj ga
+  prj _ = Nothing
+
+inject :: (sub ⊂ sup) => sub (Prog sup a ) -> Prog sup a
+inject = Op . inj
+
+project :: (sub ⊂ sup) => Prog sup a -> Maybe (sub (Prog sup a))
+project (Op s) = prj s
+project _ = Nothing
 
 
