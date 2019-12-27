40fe0e1dc4ebdd64e12e4d3285bbdfc82b9eb695
Raymond.tay <raymondtay1974@gmail.com>
2019-12-27 15:10:05 +0800

    Strategies are high-level combinators which is quite flexible

--- src/24_ConcurrentMulticore/UnderstandingStrategies.hs
+++ src/24_ConcurrentMulticore/UnderstandingStrategies.hs
@@ -21,3 +21,41 @@ simple (a, b) = do
 -- Run: runEval $ composition (1,2)
 composition = simple `dot` simple -- equivalent to simple `dot` simple `dot` r0; r0 is a no-op.
 
+fib :: Integer -> Integer
+fib n | n == 1 = 1
+  | n == 0 = 0
+  | otherwise = fib (n-1) + fib (n-2)
+
+efib :: Integer -> Integer
+efib n | n == 1 = 1
+  | n == 0 = 0
+  | otherwise = runEval $ do
+    a <- rpar (efib (n-1))
+    b <- rpar (efib (n-2))
+    return (a + b)
+
+-- Strategy version. The interesting observation here is that "using" is
+-- defined as " a -> Strategy a -> a".
+ffib :: Integer -> Integer
+ffib n | n == 0 = 0
+  | n  == 1 = 1
+  | otherwise = using (a + b) strat
+  where a = fib (n - 1)
+        b = fib (n - 2)
+        strat v = do rpar a; rseq b; return v
+
+scanP :: NFData a => Int -> (a -> a -> a) -> [a] -> [a]
+scanP d f list = concat reducedList
+  where
+    reducedList = reduce f scanList
+    scanList = map (scanl1 f) (chunk d list) `using` parList rdeepseq
+
+reduce f [] = []
+reduce f [x] = [x]
+reduce f (x:(y:xs)) = x : reduce f (map (f $ last x) y : xs)
+
+chunk :: Int -> [a] -> [[a]]
+chunk _ [] = []
+chunk n xs = as : chunk n bs
+  where (as, bs) = splitAt n xs
+
