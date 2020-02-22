75e68933f1bf33f8cd0a23e85bcff2e3b35d1485
Raymond Tay <raymondtay1974@gmail.com>
2020-02-22 12:51:14 +0800

    minor stuff

--- src/18_MonadTransformers/Reader.hs
+++ src/18_MonadTransformers/Reader.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE InstanceSigs, GeneralizedNewtypeDeriving, DeriveFunctor #-}
 
 --
 -- Demo of the MonadReader
 --
 
 import Control.Monad.Reader
+import Control.Monad.State
+
+tick :: State Int Int
+tick = do
+  i <- get
+  return $ i+1
 
 addOne :: Reader Int Int
 addOne = do i <- ask
@@ -24,5 +31,26 @@ mulTwoPlusOne = do i <- ask
 -- > 6
 -- > runReader mulTwoPlusOne 2
 -- > 5
+
+
+newtype MaybeIO a = MaybeIO { 
+  runMaybeIO :: IO (Maybe a)
+} deriving (Functor)
+
+instance Applicative MaybeIO where
+  pure x = MaybeIO (return (Just x))
+  (<*>) :: MaybeIO (a -> b) -> MaybeIO a -> MaybeIO b -- helpful, to me, to visualize the signature
+  MaybeIO f <*> MaybeIO action = MaybeIO $ do
+    g <- f
+    r <- action
+    return $ g <*> r
+
+instance Monad MaybeIO where
+  return x = MaybeIO (return (Just x))
+  MaybeIO action >>= f = MaybeIO $ do
+    result <- action
+    case result of
+        Just v -> runMaybeIO (f v)
+        Nothing -> return Nothing
 
 
