39a22da05c2cc984646636b9703984c0c2f2367a
raymondtay <raymondtay1974@gmail.com>
2020-07-14 13:36:16 +0800

    minor stuff

--- src/HaskellDesignPatterns/GenericProgramming/SoP.hs
+++ src/HaskellDesignPatterns/GenericProgramming/SoP.hs
@@ -2,6 +2,9 @@
 
 module SoP where
 
+import Numeric
+import Data.Char
+import Data.Dynamic
 
 -- It is quite normal for Haskell programmers to represent Tree or Lists ADTs
 -- in the following way; also writing functions to support this form of writing
@@ -9,11 +12,11 @@ module SoP where
 --
 data Tree a = Leaf a | Node a (Tree a) (Tree a) deriving (Show)
 
-data List' a = Nil' | Cons' a (List' a) deriving (Show)
+data List a = Nil | Cons a (List a) deriving (Show)
 
 -- Its quite common to write functions like the ones below
-sizeList Nil'         = 0
-sizeList (Cons' x xs) = 1 + (sizeList xs)
+sizeList Nil         = 0
+sizeList (Cons x xs) = 1 + (sizeList xs)
 -- count the number of nodes and leaves
 sizeTree (Leaf _) = 1
 sizeTree (Node _ l r) = 1 + (sizeTree l) + (sizeTree r)
@@ -29,7 +32,7 @@ sizeTree (Node _ l r) = 1 + (sizeTree l) + (sizeTree r)
 -- nodes and the remaining are non-terminal constructs. Therefore, we need to
 -- be able to express "Nil", "choice of either" and "combination of".
 --
--- (a) constructors that consume no arguments (e.g. Nil') are represented by
+-- (a) constructors that consume no arguments (e.g. Nil) are represented by
 -- "U".
 -- (b) next, we encode the choice between multiple constructors in the style of
 -- the "Either" type in the form taken by "Choice".
@@ -45,9 +48,9 @@ data Choice a b = L a | R b deriving (Show)
 
 data Combo a b = Combo a b deriving (Show)
 
--- RList can be read as either U or its a combination of "a (List' a)" and it
+-- RList can be read as either U or its a combination of "a (List a)" and it
 -- does not recurse (this is an important point) which i will demonstrate.
-type RList a = Choice U (Combo a (List' a))
+type RList a = Choice U (Combo a (List a))
 
 type RTree a = Choice (Combo U a) -- encodes "Leaf"
                       (Combo a (Combo (Tree a) (Tree a))) -- encodes the value of type 'a' and both branches
@@ -56,14 +59,14 @@ type RTree a = Choice (Combo U a) -- encodes "Leaf"
 
 
 -- Now, i can write functions to translate between type and representation
--- Let's start with the "List' a <-> RList a".
-fromList :: List' a -> RList a
-fromList Nil'         = L U
-fromList (Cons' x xs) = R (Combo x xs)
+-- Let's start with the "List a <-> RList a".
+fromList :: List a -> RList a
+fromList Nil         = L U
+fromList (Cons x xs) = R (Combo x xs)
 
-toList :: RList a -> List' a
-toList (L U)            = Nil'
-toList (R (Combo x xs)) = Cons' x xs
+toList :: RList a -> List a
+toList (L U)            = Nil
+toList (R (Combo x xs)) = Cons x xs
 
 fromTree :: Tree a -> RTree a
 fromTree (Leaf a)      = L (Combo U a)
@@ -76,7 +79,7 @@ toTree (R (Combo a (Combo l r))) = Node a l r
 -- There's a fundamental problem with this approach : its very shallow
 -- and the name given to this is : shallow type representation;
 -- The best way to see this is to look at how some expressions would turn
--- out. If functions were written to handle the (List' a) and (Tree a) types
+-- out. If functions were written to handle the (List a) and (Tree a) types
 -- the "usual way", everything would have worked out very well.
 --
 -- Btw, don't be too quick to dismiss it yet ;)
@@ -102,11 +105,11 @@ data TypeRepr t where
 
 -- With the "data generic" approach, "rList" is now a recursive function ;) which
 -- overcomes the shortcomings of my previous approach.
-rList :: TypeRepr a -> TypeRepr (List' a)
+rList :: TypeRepr a -> TypeRepr (List a)
 rList repr = RType (EP fromList toList) (RChoice RUnit (RCombo repr (rList repr)))
 
-intList :: List' Int
-intList = Cons' 1 (Cons' 2 (Cons' 3 Nil'))
+intList :: List Int
+intList = Cons 1 (Cons 2 (Cons 3 Nil))
 
 -- Another function which leverages pattern-matching of the smart constructors
 -- (e.g. RUnit, RChoice ...) to compute other functions
@@ -122,15 +125,15 @@ genericSize (RType ep tr) t = genericSize tr (from ep t)
 
 -- To see how "rList" would work, 
 --
--- > let xs = R (Combo 1 (Cons' 2 (Cons' 3 Nil')))
+-- > let xs = R (Combo 1 (Cons 2 (Cons 3 Nil)))
 -- >
 -- > genericSize (RChoice RUnit (RCombo RInt (rList RInt))) xs
 -- 3
 -- 
--- > genericSize (RChoice RUnit (RCombo RInt (rList RInt))) (R (Combo 2 (Cons' 3 (Cons' 4 Nil'))))
+-- > genericSize (RChoice RUnit (RCombo RInt (rList RInt))) (R (Combo 2 (Cons 3 (Cons 4 Nil))))
 -- 3
 -- 
--- > genericSize (RCombo RInt (rList RInt)) ((Combo 2 (Cons' 3 (Cons' 4 Nil')))))
+-- > genericSize (RCombo RInt (rList RInt)) ((Combo 2 (Cons 3 (Cons 4 Nil)))))
 -- 3
 
 
@@ -150,11 +153,30 @@ intTree = (Node 2 (Leaf 3) (Node 5 (Leaf 7) (Leaf 11)))
 -- 5
 --
 -- > genericSize (RChoice RUnit (rTree RInt)) (R intTree)
--- 5<Paste>
+-- 5
+
+--
+-- I like to reuse the structures created above to solve another fictitious
+-- problem : converting values of this structure to a series of bits.
+-- 
+data Bit = Z | O deriving Show -- zero or one
+compress :: TypeRepr a -> a -> [Bit]
+compress RUnit U = [Z]
+compress (RChoice rA rB) (L a) = compress rA a 
+compress (RChoice rA rB) (R b) = compress rB b
+compress (RCombo rA rB) (Combo a b) = (compress rA a) ++ (compress rB b)
+compress RInt  x = foldr (\e -> \acc -> if e == '1' then O:acc else Z:acc) [] $ showIntAtBase 2 ("01" !!) x ""
+compress RChar x = foldr (\e -> \acc -> if e == '1' then O:acc else Z:acc) [] $ showIntAtBase 2 ("01" !!) (ord x) ""
+compress (RType ep tr) t = compress tr (from ep t)
+
 
 main :: IO ()
 main = do
   putStrLn . show $ "Size of list: " ++ (show intList) ++ " is " ++ (show $ genericSize (rList RInt) intList)
   putStrLn . show $ "Size of tree: " ++ (show intTree) ++ " is " ++ (show $ genericSize (rTree RInt) intTree)
+  putStrLn . show $ "Compress a Int: "  ++ (show $ compress RInt 5)
+  putStrLn . show $ "Compress a Char: " ++ (show $ compress RChar 'Z')
+  putStrLn . show $ "Compressing a List of Ints: " ++ show (compress  (RChoice RUnit (RCombo RInt (rList RInt))) (fromList intList))
+  putStrLn . show $ "Compressing a Tree of Ints: " ++ show (compress  (rTree RInt) (intTree))
 
 
